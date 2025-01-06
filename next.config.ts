55c14c0169b2b16201a407cb6992d4a1f09142a6
unknown <ovieighosuakpo@gmail.com>
2025-01-06 12:45:43 +0100

    fix deployment build bug

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
 
-// Initialize the Vanilla Extract plugin
 const withVanillaExtract = createVanillaExtractPlugin();
 
-// Define your Next.js configuration
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
 };
 
-// Combine Vanilla Extract with Next.js configuration
 export default withVanillaExtract(nextConfig);
